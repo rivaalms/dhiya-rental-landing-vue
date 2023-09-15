b4eead7873d2384690a45aa4b49e1f7f062fe4a1
Riva Almero <riva@dev.netkromsolution.com>
2023-09-15 10:25:02 +0700

    feat: base layouts & components

---
+++ src/components/footer.vue
@@ -0,0 +1,14 @@
+<template>
+<footer class="bg-blue-700">
+   <div class="container mx-auto">
+      <div class="grid grid-cols-1 gap-4 py-12 text-white">
+         <p class="text-center">
+            Alamat | Telp | Email
+         </p>
+         <p class="text-center">
+            &copy; 2023 Dhiya Multi Niaga
+         </p>
+      </div>
+   </div>
+</footer>
+</template>
---
+++ src/components/header.vue
@@ -0,0 +1,25 @@
+<template>
+<header class="shadow">
+   <div class="container mx-auto">
+      <div class="flex justify-end">
+         <router-link 
+            to="/"
+            class="p-4 text-gray-700 hover:text-amber-500"
+         >
+            Home
+         </router-link>
+
+         <router-link
+            to="/about"
+            class="p-4 text-gray-700 hover:text-amber-500"
+         >
+            About
+         </router-link>
+      </div>
+   </div>
+</header>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+</script>
