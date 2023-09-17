05dfd9e407a9729f5e5d3e59088ff941a8b7a863
rivaalms <rvalms@icloud.com>
2023-09-17 10:12:38 +0700

    refactor: remake contact page

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -1,5 +1,5 @@
 <template>
-   <div class="h-[420px] bg-[url('../public/img/pangalengan-2.jpg')] backdrop-brightness-50 bg-center bg-no-repeat bg-cover relative">
+   <div class="h-[420px] bg-[url('/img/pangalengan-2.jpg')] backdrop-brightness-50 bg-center bg-no-repeat bg-cover relative">
       <div class="absolute h-full w-full bg-black -z-30 opacity-[25%]"></div>
       <Header class="absolute h-full w-full"/>
       <div class="container mx-auto h-full grid grid-cols-1 gap-4 place-items-center">
@@ -11,26 +11,31 @@
       </div>
    </div>
    <div class="w-2/3 mx-auto py-20">
-      <div class="p-4 grid grid-cols-3 gap-4">
-         <div class="card shadow-none">
-            <p class="text-lg pb-2 font-semibold tracking-tight text-gray-900 dark:text-white">
-               Alamat
-            </p>
-            <p>{{ store.about.address }}</p>
+      <div class="p-4 flex justify-center gap-4">
+         <div class="w-1/2">
+            <div class="w-2/3 mx-auto">
+               <img src="/img/contact.png" alt="" class="w-full">
+            </div>
          </div>
-
-         <div class="card shadow-none">
-            <p class="text-lg pb-2 font-semibold tracking-tight text-gray-900 dark:text-white">
-               Email
-            </p>
-            <p>{{ store.about.email }}</p>
-         </div>
-
-         <div class="card shadow-none">
-            <p class="text-lg pb-2 font-semibold tracking-tight text-gray-900 dark:text-white">
-               Telepon
-            </p>
-            <p>{{ store.about.phone }}</p>
+         <div class="flex flex-col justify-center">
+            <div class="mb-6">
+               <p class="text-lg mb-2 font-semibold tracking-tight text-gray-900">
+                  Alamat
+               </p>
+               <p>{{ store.about.address }}</p>
+            </div>
+            <div class="mb-6">
+               <p class="text-lg mb-2 font-semibold tracking-tight text-gray-900">
+                  Email
+               </p>
+               <p>{{ store.about.email }}</p>
+            </div>
+            <div class="mb-6">
+               <p class="text-lg mb-2 font-semibold tracking-tight text-gray-900">
+                  Telepon
+               </p>
+               <p>{{ store.about.phone }}</p>
+            </div>
          </div>
       </div>
    </div>
